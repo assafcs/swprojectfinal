e0475adc08bef09cbd33159632009902c5fdf466
Matan Eilat <matan.eilat@joytunes.com>
2016-06-03 17:28:18 +0300

    Implemented other queue methods - not tested yet.

--- SPBPriorityQueue.c
+++ SPBPriorityQueue.c
@@ -14,6 +14,8 @@
 
 // Method declaration
 SPBPQueue spBPQueueInnerCreate(SPList innerList, int maxSize);
+SPListElement spBPQueueFirstElement(SPBPQueue source);
+SPListElement spBPQueueLastElement(SPBPQueue source);
 
 struct sp_bp_queue_t {
 	SPList innerList;
@@ -87,6 +89,8 @@ SP_BPQUEUE_MSG spBPQueueEnqueue(SPBPQueue source, SPListElement element) {
 		return SP_BPQUEUE_OUT_OF_MEMORY;
 	}
 
+	bool wasFull = spBPQueueIsFull(source);
+
 	SPList list = source->innerList;
 
 	bool wasInserted = false;
@@ -115,7 +119,7 @@ SP_BPQUEUE_MSG spBPQueueEnqueue(SPBPQueue source, SPListElement element) {
 
 	if (wasInserted) {
 		// Remove the last element if needed
-		if (spListGetSize(list) > source->maxSize) {
+		if (wasFull) {
 			spListGetLast(list);
 			SP_LIST_MSG returnMSG = spListRemoveCurrent(list);
 			switch (returnMSG) {
@@ -129,7 +133,10 @@ SP_BPQUEUE_MSG spBPQueueEnqueue(SPBPQueue source, SPListElement element) {
 		}
 		return SP_BPQUEUE_SUCCESS;
 	} else {
-		if (spListGetSize(list) < source->maxSize) {
+		if (spBPQueueIsFull(source)) {
+			return SP_BPQUEUE_FULL;
+
+		} else {
 			// The item is largest than everything, insert last
 			SP_LIST_MSG returnMSG = spListInsertLast(list, elementCopy);
 			switch (returnMSG) {
@@ -139,14 +146,81 @@ SP_BPQUEUE_MSG spBPQueueEnqueue(SPBPQueue source, SPListElement element) {
 			default:
 				return SP_BPQUEUE_SUCCESS;
 			}
-		} else {
-			return SP_BPQUEUE_FULL;
 		}
 	}
 }
 
 SP_BPQUEUE_MSG spBPQueueDequeue(SPBPQueue source) {
-	if (source == NULL) return SP_BPQUEUE_ERROR;
-	else { return SP_BPQUEUE_SUCCESS; }
+	assert(source != NULL);
+
+	if (spBPQueueIsEmpty(source)) {
+		return SP_BPQUEUE_EMPTY;
+	}
+
+	spListGetFirst(source->innerList);
+	SP_LIST_MSG returnMSG = spListRemoveCurrent(source->innerList);
+
+	switch (returnMSG) {
+	case SP_LIST_INVALID_CURRENT:
+	case SP_LIST_NULL_ARGUMENT:
+		return SP_BPQUEUE_ERROR;
+	default:
+		return SP_BPQUEUE_SUCCESS;
+	}
 }
+
+SPListElement spBPQueuePeek(SPBPQueue source) {
+	assert(source != NULL);
+
+	SPListElement minElement = spBPQueueFirstElement(source);
+	return minElement == NULL ? NULL : spListElementCopy(minElement);
+}
+
+SPListElement spBPQueuePeekLast(SPBPQueue source) {
+	assert(source != NULL);
+
+	SPListElement maxElement = spBPQueueLastElement(source);
+	return maxElement == NULL ? NULL : spListElementCopy(maxElement);
+}
+
+double spBPQueueMinValue(SPBPQueue source) {
+	assert(source != NULL);
+	SPListElement minElement = spBPQueueFirstElement(source);
+	return minElement == NULL ? -1 : spListElementGetValue(minElement);
+}
+
+double spBPQueueMaxValue(SPBPQueue source) {
+	assert(source != NULL);
+	SPListElement maxElement = spBPQueueLastElement(source);
+	return maxElement == NULL ? -1 : spListElementGetValue(maxElement);
+}
+
+bool spBPQueueIsEmpty(SPBPQueue source) {
+	assert(source != NULL);
+	return spListGetSize(source->innerList) == 0;
+}
+
+bool spBPQueueIsFull(SPBPQueue source) {
+	assert(source != NULL);
+	return spListGetSize(source->innerList) == source->maxSize;
+}
+
+SPListElement spBPQueueFirstElement(SPBPQueue source) {
+	assert(source != NULL);
+
+	if (spBPQueueIsEmpty(source)) {
+		return NULL;
+	}
+	return spListGetFirst(source->innerList);
+}
+
+SPListElement spBPQueueLastElement(SPBPQueue source) {
+
+	if (spBPQueueIsEmpty(source)) {
+		return NULL;
+	}
+	return spListGetLast(source->innerList);
+}
+
+
 
